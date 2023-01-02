823654f565b91260996e22d95412efa87c533158
jtompkins415 <john.tompkins415@gmail.com>
2023-01-02 14:23:38 -0800

    write methods for Tree & BinaryTree classes

--- tree.js
+++ tree.js
@@ -15,12 +15,41 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if(!this.root) return 0;
+
+    let total = this.root.val;
+
+    function sumHelper(node){
+      for(let child of node.children){
+        total += child.val;
+        if(child.children.length > 0){
+          sumHelper(child)
+        }
+      }
+    }
+
+    sumHelper(this.root);
+    return total;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if(!this.root) return 0;
+
+    let count = this.root.val % 2 === 0 ? 1 : 0;
+
+    function countEvensHelper(node){
+      for(let child of node.children){
+        if(child.val % 2 === 0) count++;
+        if(child.children.length > 0){
+          countEvensHelper(child);
+        }
+      }
+    }
+
+    countEvensHelper(this.root);
+    return count
 
   }
 
@@ -28,7 +57,21 @@ class Tree {
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if(!this.root) return 0;
 
+    let count = this.root.val > lowerBound ? 1 : 0;
+
+    function numGreaterHelper(node){
+      for(let child of node.children){
+        if(child.val > lowerBound) count++;
+        if(child.children.length > 0){
+          numGreaterHelper(child)
+        }
+      }
+    }
+
+    numGreaterHelper(this.root);
+    return count;
   }
 }
 
